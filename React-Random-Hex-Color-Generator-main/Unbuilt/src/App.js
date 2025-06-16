b121188493eb0aaba7b6f0b38c975ad5eacb63bf
omerulusal <96357374+omerulusal@users.noreply.github.com>
2025-06-16 22:06:24 +0300

    hex sorunu cozuldu

--- React-Random-Hex-Color-Generator-main/Unbuilt/src/App.js
+++ React-Random-Hex-Color-Generator-main/Unbuilt/src/App.js
@@ -6,12 +6,11 @@ import "./App.css";
 
 function App() {
   const [hex, setHex] = useState("#ffffff");
-  function randomHex() {
+  function randomHexFunc() {
     const randomHex = "#" + Math.floor(Math.random() * 16777721).toString(16);
     // toString(16) ile gelen sayıyı hexedecimal'e çevirdim.
     setHex(randomHex);
-
-    document.body.style.backgroundColor = hex;
+    document.body.style.backgroundColor = randomHex;
   }
 
   const tost = () => {
@@ -30,7 +29,7 @@ function App() {
         <h1>{hex}</h1>
         <div>
           <button
-            onClick={randomHex}
+            onClick={randomHexFunc}
             className="btn btn-dark w-25 mx-3 btn-lg shadow-lg">
             yeni arkaplan oluştur.
           </button>
